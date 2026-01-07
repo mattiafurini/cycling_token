85600ca0ff8aab4201d90a47772c0d4cee6f7748
mattiafurinii <furini.mattia00@gmail.com>
2026-01-07 15:56:04 +0100

    c

--- contracts/CyclingToken.sol
+++ contracts/CyclingToken.sol
@@ -1,4 +1,3 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -6,13 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CyclingToken is ERC20, Ownable, ERC20Burnable {
-    // Il costruttore richiede l'indirizzo iniziale dell'owner
-    // 'msg.sender' sarà il proprietario (tu che fai il deploy)
-    constructor(uint256 initialSupply) 
-        ERC20("CyclingToken", "CYCL") 
-        Ownable(msg.sender) // Inizializza l'owner
-    {
-        // Se vuoi partire con zero token, passa 0 come initialSupply
+    constructor(
+        uint256 initialSupply
+    ) ERC20("CyclingToken", "CYCL") Ownable(msg.sender) {
         if (initialSupply > 0) {
             _mint(msg.sender, initialSupply * 10 ** decimals());
         }
@@ -20,9 +15,11 @@ contract CyclingToken is ERC20, Ownable, ERC20Burnable {
 
     event RideMinted(address indexed user, uint256 amount, string cid);
 
-    // Funzione MINT: Crea nuovi token dal nulla
-    // "onlyOwner" significa che SOLO il wallet che ha deployato il contratto può chiamarla
-    function mint(address to, uint256 amount, string memory tokenURI) public onlyOwner {
+    function mint(
+        address to,
+        uint256 amount,
+        string memory tokenURI
+    ) public onlyOwner {
         _mint(to, amount);
         emit RideMinted(to, amount, tokenURI);
     }
